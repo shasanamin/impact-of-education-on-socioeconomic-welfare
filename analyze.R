1f545160190f582dc5d2bd863a60e6fe1ca5179e
unknown <shasanamin@gmail.com>
2020-09-26 18:07:08 +0500

    improved code readablity

--- analyze.R
+++ analyze.R
@@ -5,30 +5,90 @@ source('utils.R')
 
 ## constants
 ## variables and labels for box-and-whisker plots using lattice
-# categorical variables to be created from continuous variables present in processed data file
+# categ. variables to be created from cont. variables in processed data file
 X_bw_latt <- c('SLE_Cat', 'LR_Cat', 'GEE_PC_Cat', 'EEPTGE_Cat')
-X_bw_latt_labs <- c('School Life Expectancy', 'Literacy Rate', 'Government Expenditure on Education Per Capita', 'Expenditure On Education as a Percentage of Total Government Expenditure')
-# can be any variable (column identifier) from processed data file (use exact variable names)
-Y_bw_latt <- c('HDI', 'IHDI', 'Govt_Budget_Per_Capita_USD_Nominal', 'GDP_Per_Capita_USD', 'Urban_Population', 'Percent_Pop_living_under_USD_1.9_Per_Day', 'Percent_Pop_living_under_USD_3.2_Per_Day', 'Percent_Pop_living_under_USD_5.5_Per_Day', 'Gini_Index')
-Y_bw_latt_labs <- c('Human Development Index', 'Inequality-Adjusted Human Development Index', 'Government Budget Per Capita (USD)', 'GDP Per Capita (USD)', 'Urban Population', 'Percentage Population Living Under USD 1.9 Per Day', 'Percentage Population Living Under USD 3.2 Per Day', 'Percentage Population Living Under USD 5.5 Per Day', 'Gini Index')
+X_bw_latt_labs <- c(
+  'School Life Expectancy', 
+  'Literacy Rate', 
+  'Government Expenditure on Education Per Capita', 
+  'Expenditure On Education as a Percentage of Total Government Expenditure'
+)
+# can be any variable (column identifier) from processed data file
+Y_bw_latt <- c('HDI', 
+               'IHDI', 
+               'Govt_Budget_Per_Capita_USD_Nominal', 
+               'GDP_Per_Capita_USD', 'Urban_Population', 
+               'Percent_Pop_living_under_USD_1.9_Per_Day', 
+               'Percent_Pop_living_under_USD_3.2_Per_Day', 
+               'Percent_Pop_living_under_USD_5.5_Per_Day', 
+               'Gini_Index')
+Y_bw_latt_labs <- c('Human Development Index', 
+                    'Inequality-Adjusted Human Development Index', 
+                    'Government Budget Per Capita (USD)', 
+                    'GDP Per Capita (USD)', 
+                    'Urban Population', 
+                    'Percentage Population Living Under USD 1.9 Per Day', 
+                    'Percentage Population Living Under USD 3.2 Per Day', 
+                    'Percentage Population Living Under USD 5.5 Per Day', 
+                    'Gini Index')
 
 ## variables and labels for scatter plots with linear fit using ggplot
 # can be any variable from processed data file
-X_sf_gg <- c('Expenditure_On_Education_As_A_Percentage_Of_Total_Government_Expenditure', 'GEE_PC', 'Literacy_Rate')
-X_sf_gg_labs <- c('Expenditure on Education as a Percentage of Total Government Expenditure', 'Government Expenditure on Education Per Capita', 'Literacy Rate')
-Y_sf_gg <- c('GDP_Per_Capita_USD', 'Govt_Budget_Per_Capita_USD_Nominal', 'Gini_Index', 'HDI', 'Percent_Pop_living_under_USD_5.5_Per_Day', 'Urban_Population')
-Y_sf_gg_labs <- c('GDP per Capita (USD)', 'Government Budget Per Capita', 'Gini Index', 'Human Development Index', 'Population Living Under 5.5 USD Per Day', 'Urban Population')
+X_sf_gg <- c(
+  'Expenditure_On_Education_As_A_Percentage_Of_Total_Government_Expenditure', 
+  'GEE_PC', 
+  'Literacy_Rate'
+)
+X_sf_gg_labs <- c(
+  'Expenditure on Education as a Percentage of Total Government Expenditure', 
+  'Government Expenditure on Education Per Capita', 
+  'Literacy Rate'
+)
+Y_sf_gg <- c('GDP_Per_Capita_USD', 
+             'Govt_Budget_Per_Capita_USD_Nominal', 
+             'Gini_Index', 
+             'HDI', 
+             'Percent_Pop_living_under_USD_5.5_Per_Day', 
+             'Urban_Population')
+Y_sf_gg_labs <- c('GDP per Capita (USD)', 
+                  'Government Budget Per Capita', 
+                  'Gini Index', 
+                  'Human Development Index', 
+                  'Population Living Under 5.5 USD Per Day', 
+                  'Urban Population')
 
 ## variables and labels for density plots using ggplot
 # can be any variable from processed data file
-X_density_gg <- c('Literacy_Rate', 'School_Life_Expectancy', 'GEE_PC', 'Govt_Budget_Per_Capita_USD_Nominal', 'Expenditure_On_Education_As_A_Percentage_Of_Total_Government_Expenditure', 'Urban_Population', 'Population_By_Bachelors_Completed')
-X_density_labs <- c('Literacy Rate', 'School Life Expectancy', 'Government Expenditure on Education Per Capita', 'Government Budget Per Capita (USD)', 'Expenditure on Education as a Percentage of Total Government Expenditure', 'Urban Population', 'Population By Bachelors Completed')
+X_density_gg <- c('Literacy_Rate', 
+                  'School_Life_Expectancy', 
+                  'GEE_PC', 
+                  'Govt_Budget_Per_Capita_USD_Nominal', 
+                  'Expenditure_On_Education_As_A_Percentage_Of_Total_Government_Expenditure', 
+                  'Urban_Population', 
+                  'Population_By_Bachelors_Completed')
+X_density_labs <- c(
+  'Literacy Rate', 
+  'School Life Expectancy', 
+  'Government Expenditure on Education Per Capita', 
+  'Government Budget Per Capita (USD)', 
+  'Expenditure on Education as a Percentage of Total Government Expenditure', 
+  'Urban Population', 
+  'Population By Bachelors Completed'
+)
 
 ## variables for hypothesis testing, i.e., to perform statistical inference
 X_SI <- c('LR_Cat', 'SLE_Cat', 'GEE_PC_Cat')
-Y_SI <- c('GDP_Per_Capita_USD', 'Govt_Budget_Per_Capita_USD_Nominal', 'HDI', 'Urban_Population', 'Percent_Pop_living_under_USD_5.5_Per_Day')
+Y_SI <- c('GDP_Per_Capita_USD', 
+          'Govt_Budget_Per_Capita_USD_Nominal', 
+          'HDI', 
+          'Urban_Population', 
+          'Percent_Pop_living_under_USD_5.5_Per_Day')
 
-
+## main function to analyze (perform EDA and hypothesis testing) processed data
+# takes paths of directory with processed data and
+# directory where graphs would be saved as command line inputs,
+# and outputs results of performed hypothesis testing while 
+# saving produced graphs in provided directory for saving
 main <- function(){
   args <- commandArgs(trailingOnly = TRUE)
   data_path <- args[1]
@@ -44,25 +104,48 @@ main <- function(){
   df <- read.csv(data_path)
 
   ## handle misread data
-  df$Population <- as.numeric(gsub(',', '', as.character(df$Population)))
-  df$Government_Expenditure_On_Education_In_USD_Millions <- as.numeric(gsub(',', '', as.character(df$Government_Expenditure_On_Education_In_USD_Millions)))
-  df$GDP_Per_Capita_USD <- as.numeric(gsub(',', '', as.character(df$GDP_Per_Capita_USD)))
-  df$Govt_Budget_Per_Capita_USD_Nominal <- as.numeric(gsub(',', '', as.character(df$Govt_Budget_Per_Capita_USD_Nominal)))
-  df$Percent_Pop_living_under_USD_1.9_Per_Day <- as.numeric(gsub('%', '', as.character(df$Percent_Pop_living_under_USD_1.9_Per_Day)))
-  df$Percent_Pop_living_under_USD_3.2_Per_Day <- as.numeric(gsub('%', '', as.character(df$Percent_Pop_living_under_USD_3.2_Per_Day)))
-  df$Percent_Pop_living_under_USD_5.5_Per_Day <- as.numeric(gsub('%', '', as.character(df$Percent_Pop_living_under_USD_5.5_Per_Day)))
-  df$Urban_Population <- as.numeric(gsub('%', '', as.character(df$Urban_Population)))
+  df$Population <- as.numeric(
+    gsub(',', '', as.character(df$Population))
+  )
+  df$Government_Expenditure_On_Education_In_USD_Millions <- as.numeric(
+    gsub(',', '', as.character(df$Government_Expenditure_On_Education_In_USD_Millions))
+  )
+  df$GDP_Per_Capita_USD <- as.numeric(
+    gsub(',', '', as.character(df$GDP_Per_Capita_USD))
+  )
+  df$Govt_Budget_Per_Capita_USD_Nominal <- as.numeric(
+    gsub(',', '', as.character(df$Govt_Budget_Per_Capita_USD_Nominal))
+  )
+  df$Percent_Pop_living_under_USD_1.9_Per_Day <- as.numeric(
+    gsub('%', '', as.character(df$Percent_Pop_living_under_USD_1.9_Per_Day))
+  )
+  df$Percent_Pop_living_under_USD_3.2_Per_Day <- as.numeric(
+    gsub('%', '', as.character(df$Percent_Pop_living_under_USD_3.2_Per_Day))
+  )
+  df$Percent_Pop_living_under_USD_5.5_Per_Day <- as.numeric(
+    gsub('%', '', as.character(df$Percent_Pop_living_under_USD_5.5_Per_Day))
+  )
+  df$Urban_Population <- as.numeric(
+    gsub('%', '', as.character(df$Urban_Population))
+  )
   df$GEE_PC <- df$Government_Expenditure_On_Education_In_USD_Millions / df$Population
 
-  ## transform (continuous) data to (categorical) quantiles for exploratory data analysis (especially for boxplots) and hypothesis testing
+  ## transform (continuous) data to (categorical) quantiles 
+  # for exploratory data analysis (esp. for boxplots) and hypothesis testing
   df <- add_var_quantiles(df, 'Literacy_Rate', 'LR_Cat')
   df <- add_var_quantiles(df, 'School_Life_Expectancy', 'SLE_Cat')
   df <- add_var_quantiles(df, 'GEE_PC', 'GEE_PC_Cat')
-  df <- add_var_quantiles(df, 'Expenditure_On_Education_As_A_Percentage_Of_Total_Government_Expenditure', 'EEPTGE_Cat')
+  df <- add_var_quantiles(
+    df, 
+    'Expenditure_On_Education_As_A_Percentage_Of_Total_Government_Expenditure', 
+    'EEPTGE_Cat'
+  )
 
   ## perform exploratory data analysis
-  bwplots_lattice(df=df, X=X_bw_latt, X_labs=X_bw_latt_labs, Y=Y_bw_latt, Y_labs=Y_bw_latt_labs)
-  plots_gg(plt_type='scatter_fit', df=df, X=X_sf_gg, X_labs=X_sf_gg_labs, Y=Y_sf_gg, Y_labs= Y_sf_gg_labs)
+  bwplots_lattice(df=df, X=X_bw_latt, X_labs=X_bw_latt_labs, 
+                  Y=Y_bw_latt, Y_labs=Y_bw_latt_labs)
+  plots_gg(plt_type='scatter_fit', df=df, X=X_sf_gg, X_labs=X_sf_gg_labs, 
+           Y=Y_sf_gg, Y_labs= Y_sf_gg_labs)
   plots_gg(plt_type='density', df=df, X=X_density_gg, X_labs=X_density_labs)
 
   ## perform hypothesis testing
--- process.R
+++ process.R
@@ -11,23 +11,30 @@ URLs <- vector(mode='list', length=6)
 xpaths <- vector(mode='list', length=6)
 names(URLs) <- c('pop', 'hdi', 'gdp', 'gbp', 'pov', 'equ')
 names(xpaths) <- c('pop', 'hdi', 'gdp', 'gbp', 'pov', 'equ')
-URLs[[1]] <- 'https://www.worldometers.info/world-population/population-by-country/'
+URLs[[1]] <- 
+  'https://www.worldometers.info/world-population/population-by-country/'
+URLs[[2]] <- 
+  'https://en.wikipedia.org/wiki/List_of_countries_by_inequality-adjusted_HDI'
+URLs[[3]] <- 
+  'https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)_per_capita'
+URLs[[4]] <- 
+  'https://en.wikipedia.org/wiki/List_of_countries_by_government_budget_per_capita'
+URLs[[5]] <- 
+  'https://en.wikipedia.org/wiki/List_of_countries_by_percentage_of_population_living_in_poverty'
+URLs[[6]] <- 
+  'https://en.wikipedia.org/wiki/List_of_countries_by_income_equality'
 xpaths[[1]] <- '//*[@id="example2"]'
-URLs[[2]] <- 'https://en.wikipedia.org/wiki/List_of_countries_by_inequality-adjusted_HDI'
 xpaths[[2]] <- '//*[@id="mw-content-text"]/div/table'
-URLs[[3]] <- 'https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)_per_capita'
 xpaths[[3]] <- '//*[@id="mw-content-text"]/div/table/tbody/tr[2]/td[2]/table'
-URLs[[4]] <- 'https://en.wikipedia.org/wiki/List_of_countries_by_government_budget_per_capita'
 xpaths[[4]] <- '//*[@id="mw-content-text"]/div/table'
-URLs[[5]] <- 'https://en.wikipedia.org/wiki/List_of_countries_by_percentage_of_population_living_in_poverty'
 xpaths[[5]] <- '//*[@id="mw-content-text"]/div/table[2]'
-URLs[[6]] <- 'https://en.wikipedia.org/wiki/List_of_countries_by_income_equality'
 xpaths[[6]] <- '//*[@id="UNandCIA"]'
 
 
 ## main function for (pre)processing data from multiple sources
-# takes path of directory with data files and output directory as command line inputs
-# and saves a .csv file containing information from these files as well as that obtained through web scraping in provided output directory
+# takes paths of directory with data files and output directory as command line inputs,
+# and saves a .csv file containing information from these files as well as
+# information obtained through web scraping in provided output directory
 main <- function(){
   args <- commandArgs(trailingOnly = TRUE)
   data_path <- args[1]
@@ -54,12 +61,23 @@ main <- function(){
 
   ## perform web scraping to gather data from online sources
   # additional geographic and demographic information
-  countries_more_data <- get_table_as_df(as.character(URLs['pop']), as.character(xpaths['pop']))
-  countries_more_data <- subset(countries_more_data, select=c('Country..or.dependency.', 'Population..2020.', 'Land.Area..Km².', 'Density..P.Km².', 'Urban.Pop..'))
-  colnames(countries_more_data) <- c('Country', 'Population', 'Land_Area_km2', 'Density_per_km2', 'Urban_Population')
+  countries_more_data <- get_table_as_df(as.character(URLs['pop']), 
+                                         as.character(xpaths['pop']))
+  countries_more_data <- subset(countries_more_data,
+                                select=c('Country..or.dependency.', 
+                                         'Population..2020.', 
+                                         'Land.Area..Km².', 
+                                         'Density..P.Km².', 
+                                         'Urban.Pop..'))
+  colnames(countries_more_data) <- c('Country', 
+                                     'Population', 
+                                     'Land_Area_km2', 
+                                     'Density_per_km2', 
+                                     'Urban_Population')
 
   # human development index and inequality adjusted human development index
-  i_hdi <- get_table_as_df(as.character(URLs['hdi']), as.character(xpaths['hdi']))
+  i_hdi <- get_table_as_df(as.character(URLs['hdi']), 
+                           as.character(xpaths['hdi']))
   true_header <- unlist(i_hdi[1,], use.names = FALSE)
   true_header[1] <- 'HDI_Rank'
   i_hdi <- i_hdi[-1,]
@@ -67,33 +85,48 @@ main <- function(){
   i_hdi <- i_hdi[,c('Country', 'HDI', 'IHDI')]
 
   # gross domestic product per capita  
-  gdp_pc <- get_table_as_df(as.character(URLs['gdp']), as.character(xpaths['gdp']))
+  gdp_pc <- get_table_as_df(as.character(URLs['gdp']), 
+                            as.character(xpaths['gdp']))
   gdp_pc <- gdp_pc[,c('Country.Territory', 'US.')]
   colnames(gdp_pc) <- c('Country', 'GDP_Per_Capita_USD')
 
   # government budget per capita
-  gb_pc <- get_table_as_df(as.character(URLs['gbp']), as.character(xpaths['gbp']))
+  gb_pc <- get_table_as_df(as.character(URLs['gbp']), 
+                           as.character(xpaths['gbp']))
   gb_pc <- gb_pc[,c('Country', 'Government.budget.per.capita..US...nominal.')]
   colnames(gb_pc) <- c('Country', 'Govt_Budget_Per_Capita_USD_Nominal')
 
   # population living below poverty level
-  below_poverty <- get_table_as_df(as.character(URLs['pov']), as.character(xpaths['pov']))
+  below_poverty <- get_table_as_df(as.character(URLs['pov']), 
+                                   as.character(xpaths['pov']))
   below_poverty <- below_poverty[,-which(colnames(below_poverty) == 'Year')]
-  colnames(below_poverty) <- c('Country', 'Percent_Pop_living_under_USD_1.9_Per_Day', 'Percent_Pop_living_under_USD_3.2_Per_Day', 'Percent_Pop_living_under_USD_5.5_Per_Day', 'Continent')
+  colnames(below_poverty) <- c('Country', 
+                               'Percent_Pop_living_under_USD_1.9_Per_Day', 
+                               'Percent_Pop_living_under_USD_3.2_Per_Day', 
+                               'Percent_Pop_living_under_USD_5.5_Per_Day', 
+                               'Continent')
 
   # gini index
-  inequality <- get_table_as_df(as.character(URLs['equ']), as.character(xpaths['equ']))
+  inequality <- get_table_as_df(as.character(URLs['equ']), 
+                                as.character(xpaths['equ']))
   inequality <- inequality[3:dim(inequality)[1],]
   inequality <- subset(inequality, select=c('Country', 'World.Bank.Gini.4.'))
   colnames(inequality) <- c('Country', 'Gini_Index')
 
   ## merge available data into a single main dataframe
-  # only include countries present in all individual dataframes (can update this if desired)
+  # only include countries present in all individual dataframes
   df_int <- merge_by_country(c(excel_dfs, csv_dfs), data_year, select_year=TRUE)
-  # remove year column since it would be added again on next merge_by_country call
+  # remove year column as it would be added again on next merge_by_country call
   df_int <- df_int[,-2]
-  df_processed <- merge_by_country(c('df_int', json_dfs, 'countries_more_data', 'i_hdi', 'gdp_pc', 'below_poverty', 'inequality'), data_year, select_year=FALSE, env=main_env)
-  # budget values typically from 2017, which may not be very relevant for all years
+  df_processed <- merge_by_country(c('df_int', 
+                                     json_dfs, 
+                                     'countries_more_data', 
+                                     'i_hdi', 
+                                     'gdp_pc',
+                                     'below_poverty',
+                                     'inequality'),
+                                   data_year, select_year=FALSE, env=main_env)
+  # budget values typically from 2017, which may not be relevant for all years
   if(data_year=='2017'| data_year=='2018'){
     df_processed <- merge(df_processed, gb_pc, by='Country')
   }
@@ -103,8 +136,11 @@ main <- function(){
     dir.create(out_dir)
   }
   out_file <- paste('data', data_year, '_processed.csv', sep='')
-  write.csv(df_processed, paste(out_dir, '/data', data_year, '_processed.csv', sep=''), row.names=FALSE)
-  cat(paste0('Processing Finished!\n', out_file, ' is now available in ', out_dir))
+  write.csv(df_processed, 
+            paste(out_dir, '/data', data_year, '_processed.csv', sep=''), 
+            row.names=FALSE)
+  cat(paste0('Processing Finished!\n', out_file, 
+             ' is now available in ', out_dir))
 }
 
 
